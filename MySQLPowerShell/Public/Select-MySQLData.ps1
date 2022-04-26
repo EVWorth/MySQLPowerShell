e7bcc6eda555e518141f16437df6f527eefb24cd
Elliot Worth <ElliotWorth@Protonmail.com>
2022-04-26 15:47:05 -0500

    Updated to ordered Hashtable.

--- MySQLPowerShell/Public/Select-MySQLData.ps1
+++ MySQLPowerShell/Public/Select-MySQLData.ps1
@@ -11,48 +11,49 @@ function Select-MySQLData {
     .LINK
     #>
     
-        [CmdletBinding()]
-        param (
-            [Parameter(ValueFromPipeline)]
-            [MySql.Data.MySqlClient.MySqlConnection]$Session,
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline)]
+        [MySql.Data.MySqlClient.MySqlConnection]$Session,
 
-            [Parameter()]
-            [String]$Select = '*',
+        [Parameter()]
+        [String]$Select = '*',
 
-            [Parameter(Mandatory)]
-            [String]$From,
+        [Parameter(Mandatory)]
+        [String]$From,
 
-            [Parameter()]
-            [String]$Where
+        [Parameter()]
+        [String]$Where
 
-            #Build out other SQL statement options
-        )
+        #Build out other SQL statement options
+    )
         
-        begin {}
+    begin {}
         
-        process {
-            $Parameters = @{
-                Select = $Select;
-                From = $From;
-                Where = $Where;
-            }
+    process {
+        $Parameters = [ordered]@{
+            Select = $Select;
+            From   = $From;
+            Where  = $Where;
+        }
 
-            #Build Connection String
-            [String]$Statement = ''
-            ForEach ($Key in $Parameters.Keys) {
-                If ( -Not $Null -eq $Parameters["$Key"]) {
-                    $Statement += "$Key $($Parameters["$Key"]) "
-                }
-            }
-            
-            Try {
-                Return $Session | Open-MySQLSession | Invoke-MySQLStatement -Statement $Statement -ReturnData -Confirm:$False
-            } Catch {
-                Throw
+        #Build Connection String
+        [String]$Statement = ''
+        ForEach ($Key in $Parameters.Keys) {
+            If ( -Not $Null -eq $Parameters["$Key"]) {
+                $Statement += "$Key $($Parameters["$Key"]) "
             }
         }
-        
-        end {
+            
+        Try {
+            Return $Session | Open-MySQLSession | Invoke-MySQLStatement -Statement $Statement -ReturnData -Confirm:$False
+        }
+        Catch {
+            Throw
         }
     }
+        
+    end {
+    }
+}
     
